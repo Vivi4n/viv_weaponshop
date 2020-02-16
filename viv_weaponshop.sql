0c2b2fc1d707829da9784edbcb0fc9da4cc47d04
Vivian Leigh <ypetrov@godaddy.com>
2020-02-16 08:37:05 +0200

    General update of annoying French thingies

--- viv_weaponshop.sql
+++ viv_weaponshop.sql
@@ -10,7 +10,7 @@ CREATE TABLE `weashops` (
 );
 
 INSERT INTO `licenses` (`type`, `label`) VALUES
-	('weapon', "Permis de port d'arme")
+	('weapon', "Weapon license")
 ;
 
 INSERT INTO `weashops` (`zone`, `item`, `price`) VALUES
